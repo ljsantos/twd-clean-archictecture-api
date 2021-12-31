beb05bd2a3c202149bbf80801cc55243c6cb2c5b
Lucas J Santos <lucas@manjaro.local>
2021-12-31 15:12:21 -0300

    feat: should return user if it is found in repository

--- src/use_cases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/use_cases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -11,6 +11,12 @@ export class InMemoryUserRepository implements UserRepository {
 
 
     async findUserByEmail(email: string): Promise<UserData> {
+        const users = this.repository.filter((user) => {
+            return user.email === email
+        })
+        if (users.length > 0){
+            return users[0]
+        }
         return null
     }
 }
