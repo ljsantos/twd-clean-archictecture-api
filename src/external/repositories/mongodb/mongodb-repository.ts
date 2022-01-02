0413822d9ff25b10048bc7434a5a64f7af0efb71
Lucas J Santos <lucas@manjaro.local>
2022-01-02 18:03:21 -0300

    feat: add user e verifi existence using mongodb

---
+++ src/external/repositories/mongodb/mongodb-repository.ts
@@ -0,0 +1,31 @@
+import { UserData } from '@/entities'
+import { UserRepository } from '@/use_cases/ports'
+import { MongoHelper } from './helper'
+
+export class MongodbUserRepository implements UserRepository {
+  async add (user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection('users')
+    const exists = await this.exists(user)
+    if (!exists) {
+      await userCollection.insertOne(user)
+    }
+  }
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const userCollection = MongoHelper.getCollection('users')
+    const result = await userCollection.findOne({ email: email })
+    return (result as unknown) as UserData
+  }
+
+  async findAllUsers (): Promise<UserData[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async exists (user: UserData): Promise<boolean> {
+    const result = await this.findUserByEmail(user.email)
+    if (result === null) {
+      return false
+    }
+    return true
+  }
+}
