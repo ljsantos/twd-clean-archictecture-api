fe8591ffb9fe0df9040deb5a774fdbbdb2005916
Lucas J Santos <lucas@manjaro.local>
2022-01-01 23:55:48 -0300

    chore: add index.ts for each folder

--- src/use_cases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/use_cases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -1,9 +1,9 @@
-import { InvalidEmailError } from '../../../src/entities/errors/invalid-email-error'
-import { InvalidNameError } from '../../../src/entities/errors/invalid-name-error'
-import { User } from '../../../src/entities/user'
-import { UserData } from '../../../src/entities/user-data'
-import { Either, left, right } from '../../../src/shared/either'
-import { UserRepository } from '../../../src/use_cases/ports/user-repository'
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../entities/errors/invalid-name-error'
+import { User } from '../../entities/user'
+import { UserData } from '../../entities/user-data'
+import { Either, left, right } from '../../shared/either'
+import { UserRepository } from '../ports/user-repository'
 
 export class RegisterUserOnMailingList {
   private readonly userRepo: UserRepository
