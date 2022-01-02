757cddd48dc117486c361e81bd4a0e3ebaa84a0b
Lucas J Santos <lucas@manjaro.local>
2022-01-02 07:35:10 -0300

    feat: web controller should return 201 when user is created

---
+++ src/web-controllers/register-user-controller.ts
@@ -0,0 +1,20 @@
+import { UserData } from '@/entities'
+import { RegisterUserOnMailingList } from '@/use_cases/register-user-on-mailing-list'
+import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
+import { created } from '@/web-controllers/utils'
+
+export class RegisterUserController {
+  private readonly usecase: RegisterUserOnMailingList
+
+  constructor (usecase: RegisterUserOnMailingList) {
+    this.usecase = usecase
+  }
+
+  public async handle (request: HttpRequest): Promise<HttpResponse> {
+    const userData: UserData = request.body
+    const response = await this.usecase.RegisterUserOnMailingList(userData)
+    if (response.isRight()) {
+      return created(response.value)
+    }
+  }
+}
