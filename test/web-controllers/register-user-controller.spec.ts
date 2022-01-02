372a74be1f8499ef62c045e0b39702c1a0c04954
Lucas J Santos <lucas@manjaro.local>
2022-01-02 08:11:21 -0300

    test: web controller should return status code 400 when request contais invalid name

--- test/web-controllers/register-user-controller.spec.ts
+++ test/web-controllers/register-user-controller.spec.ts
@@ -4,6 +4,7 @@ import { RegisterUserOnMailingList } from '@/use_cases/register-user-on-mailing-
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
 import { RegisterUserController } from '@/web-controllers'
 import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { InvalidNameError } from '@/entities/errors'
 
 describe('Sign up web controller', () => {
   test('should return status code 201 when request contais valid user data', async () => {
@@ -20,5 +21,21 @@ describe('Sign up web controller', () => {
     const response: HttpResponse = await controller.handle(validRequest)
     expect(response.statusCode).toEqual(201)
     expect(response.body).toEqual(validRequest.body)
+  })
+
+  test('should return status code 400 when request contais invalid name', async () => {
+    const requestWithInvalidName: HttpRequest = {
+      body: {
+        name: 'E  ',
+        email: 'any@mail.com'
+      }
+    }
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const controller: RegisterUserController = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(requestWithInvalidName)
+    expect(response.statusCode).toEqual(400)
+    expect(response.body).toBeInstanceOf(InvalidNameError)
   })
 })
