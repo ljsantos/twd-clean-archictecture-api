d07f444ec438df667f37f4993a21a305c7e46db8
Lucas J Santos <lucas@manjaro.local>
2022-01-02 22:58:27 -0300

    test: email user with valid name and email address

---
+++ test/usecases/send-email/send-email.spec.ts
@@ -0,0 +1,45 @@
+import { Either, Right, right } from '@/shared'
+import { MailServiceError } from '@/use_cases/errors'
+import { SendEmail } from '@/use_cases/send-mail'
+import { EmailOptions, EmailService } from '@/use_cases/send-mail/ports'
+
+class MailServiceStub implements EmailService {
+  async send (mailOptions: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+    return right(mailOptions)
+  }
+}
+
+describe('Send email to user', () => {
+  const attachmentFilePath = '../resources/text.txt'
+  const fromName = 'Test'
+  const fromEmail = 'from_email@mail.com'
+  const toName = 'any_name'
+  const toEmail = 'any_email@mail.com'
+  const subject = 'Test e-mail'
+  const emailBody = 'Hello world attachament test'
+  const emailBodyHtml = '<b>Hellow world attachment test</b>'
+  const attachment = [{
+    filename: attachmentFilePath,
+    contentType: 'text/plain'
+  }]
+
+  const mailOptions: EmailOptions = {
+    host: 'test',
+    port: 867,
+    username: 'test',
+    password: 'passwd',
+    from: fromName + ' ' + fromEmail,
+    to: toName + ' <' + toEmail + '>',
+    subject: subject,
+    text: emailBody,
+    html: emailBodyHtml,
+    attachments: attachment
+  }
+  test('should email user with valid name and email address', async () => {
+    const mailServiceStub = new MailServiceStub()
+    const useCase = new SendEmail(mailServiceStub)
+    const response = await useCase.perform(mailOptions)
+    expect(response).toBeInstanceOf(Right)
+    expect(response.value).toEqual(mailOptions)
+  })
+})
