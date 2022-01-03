d6c7546c894871d5119a7cae9b5f7d740a5576fa
Lucas J Santos <lucas@manjaro.local>
2022-01-02 23:38:05 -0300

    feat: not to try email with invalid email address

--- src/use_cases/send-mail/send-email.ts
+++ src/use_cases/send-mail/send-email.ts
@@ -1,5 +1,6 @@
-import { UserData } from '@/entities'
-import { left, right } from '@/shared'
+import { User, UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { Either, left, right } from '@/shared'
 import { UseCase } from '@/use_cases/ports'
 import { EmailOptions, EmailService } from '@/use_cases/send-mail/ports'
 
@@ -13,6 +14,11 @@ export class SendEmail implements UseCase {
 
   async perform (request: UserData): Promise<any> {
     const user: UserData = request
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
     this.emailOptions.to = user.name + ' <' + user.email + '>'
     const response = await this.emailService.send(this.emailOptions)
     if (response.isLeft()) {
