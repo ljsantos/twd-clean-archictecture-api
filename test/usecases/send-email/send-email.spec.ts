f8e3e4140d8046a9828afdc0d94ca3329f00807e
santosjl <santos.j.lucas@gmail.com>
2022-01-09 21:57:22 -0300

    test: remove name and email validation from Send Mail Use Case

--- test/usecases/send-email/send-email.spec.ts
+++ test/usecases/send-email/send-email.spec.ts
@@ -1,5 +1,4 @@
-import { UserData } from '@/entities'
-import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { User, UserData } from '@/entities'
 import { Either, left, Left, Right, right } from '@/shared'
 import { MailServiceError } from '@/usecases/errors'
 import { SendEmail } from '@/usecases/send-mail'
@@ -46,46 +45,24 @@ describe('Send email to user', () => {
   test('should email user with valid name and email address', async () => {
     const mailServiceStub = new MailServiceStub()
     const useCase = new SendEmail(mailServiceStub, mailOptions)
-    const user: UserData = {
+    const userData: UserData = {
       name: 'any_name',
       email: 'any@mail.com'
     }
+    const user = User.create(userData).value as unknown as User
     const response = await useCase.perform(user)
     expect(response).toBeInstanceOf(Right)
-    expect((response.value as EmailOptions).to).toEqual(user.name + ' <' + user.email + '>')
-  })
-
-  test('should not to try email with invalid email address', async () => {
-    const mailServiceStub = new MailServiceStub()
-    const useCase = new SendEmail(mailServiceStub, mailOptions)
-    const user: UserData = {
-      name: 'any_name',
-      email: 'invalidmail.com'
-    }
-    const response = await useCase.perform(user)
-    expect(response).toBeInstanceOf(Left)
-    expect((response.value as InvalidEmailError).message).toEqual('Invalid e-mail: ' + user.email + '.')
-  })
-
-  test('should not to try email with invalid name', async () => {
-    const mailServiceStub = new MailServiceStub()
-    const useCase = new SendEmail(mailServiceStub, mailOptions)
-    const user: UserData = {
-      name: 'a',
-      email: 'any@mail.com'
-    }
-    const response = await useCase.perform(user)
-    expect(response).toBeInstanceOf(Left)
-    expect((response.value as InvalidNameError).message).toEqual('Invalid name: ' + user.name + '.')
+    expect((response.value as EmailOptions).to).toEqual(userData.name + ' <' + userData.email + '>')
   })
 
   test('should return error when email service fails', async () => {
     const failingMailServiceStub = new FailingMailServiceStub()
     const useCase = new SendEmail(failingMailServiceStub, mailOptions)
-    const user: UserData = {
+    const userData: UserData = {
       name: 'any name',
       email: 'any@mail.com'
     }
+    const user = User.create(userData).value as unknown as User
     const response = await useCase.perform(user)
     expect(response).toBeInstanceOf(Left)
     expect((response.value as MailServiceError).message).toEqual('Mail service error.')
