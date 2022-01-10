0f40dcf6949755d51c3f7684e28cca9444456c62
santosjl <santos.j.lucas@gmail.com>
2022-01-09 23:51:47 -0300

    feat: return ok if email is sent

---
+++ src/external/mail-services/nodemailer-email-service.ts
@@ -0,0 +1,28 @@
+import { Either, right } from '@/shared'
+import { MailServiceError } from '@/usecases/errors'
+import { EmailOptions, EmailService } from '@/usecases/send-mail/ports'
+import * as nodemailer from 'nodemailer'
+
+export class NodemailerMailService implements EmailService {
+  async send (options: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+    const transporter = nodemailer.createTransport(
+      {
+        host: options.host,
+        port: options.port,
+        auth: {
+          user: options.username,
+          pass: options.password
+        }
+      }
+    )
+    await transporter.sendMail({
+      from: options.from,
+      to: options.to,
+      subject: options.subject,
+      text: options.text,
+      html: options.html,
+      attachments: options.attachments
+    })
+    return right(options)
+  }
+}
