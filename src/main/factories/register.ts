1d0d94fe562039d1b077f346d8ffb2affc04c196
santosjl <santos.j.lucas@gmail.com>
2022-01-18 00:15:05 -0300

    fix: fix controller name

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,4 +1,4 @@
-import { RegisterAndSendEmailController } from '@/web-controllers'
+import { RegisterUserController } from '@/web-controllers'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { MongodbUserRepository } from '@/external/repositories/mongodb'
 import { SendEmail } from '@/usecases/send-mail'
@@ -6,12 +6,12 @@ import { NodemailerMailService } from '@/external/mail-services'
 import { getEmailOptions } from '../config/email'
 import { RegisterAndSendEmail } from '@/usecases/register-and-send-email'
 
-export const makeRegisterUserAndSendEmailController = (): RegisterAndSendEmailController => {
+export const makeRegisterUserAndSendEmailController = (): RegisterUserController => {
   const mongoDbUserRepository = new MongodbUserRepository()
   const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongoDbUserRepository)
   const emailService = new NodemailerMailService()
   const sendEmailUseCase = new SendEmail(emailService, getEmailOptions())
   const registerAndSendEmailUseCase = new RegisterAndSendEmail(registerUserOnMailingListUseCase, sendEmailUseCase)
-  const registerAndSendEmailController = new RegisterAndSendEmailController(registerAndSendEmailUseCase)
+  const registerAndSendEmailController = new RegisterUserController(registerAndSendEmailUseCase)
   return registerAndSendEmailController
 }
